972ed2c5785ceca7d7e18021fe26515838cb41f0
Ian Sweet <ian@correctcomputation.com>
2020-03-09 14:19:47 -0400

    skeleton

---
+++ clang/tools/recursive-funs-interview/RecursiveFunsIntv.cpp
@@ -0,0 +1,22 @@
+// Declares clang::SyntaxOnlyAction.
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang::tooling;
+using namespace llvm;
+
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static cl::extrahelp MoreHelp("\nMore help text...\n");
+
+int main(int argc, const char **argv) {
+    CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+    ClangTool Tool(OptionsParser.getCompilations(),
+                   OptionsParser.getSourcePathList());
+
+    // FIXME
+    return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+}
