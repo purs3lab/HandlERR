fc96f232feb0fc3fea6ec81d93c8458c89948682
Aaron Eline <aeline@umd.edu>
2021-01-12 14:17:49 -0500

    Cleanup

--- clang/include/clang/3C/Duplicate.h
+++ clang/include/clang/3C/Duplicate.h
@@ -1,18 +1,23 @@
 #ifndef LLVM_DUPLCIATE_H
 #define LLVM_DUPLCIATE_H
 
-#include "clang/AST/ASTContext.h"
 #include "ProgramInfo.h"
+#include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/Stmt.h"
 #include "clang/Rewrite/Core/Rewriter.h"
+#include <getopt.h>
 
 typedef std::function<bool (clang::VarDecl*)> selector;
 
-//TODO this should be a bool for error reporting
-void create_duplicate(clang::ASTContext &Context, clang::Rewriter &R,
-                      ProgramInfo &Info,
-                      clang::Decl *FD, selector P);
+// Create a duplicate of a variable.
+// ToScan is the context to search for the variable
+// P is a predicate that decides which variable to make a duplicate of
+// Returns the name of the newly created variable
+// or None if no variable matching P is found
+Option<std::string> createDuplicate(clang::ASTContext &Context,
+                                    clang::Rewriter &R, ProgramInfo &Info,
+                                    clang::Decl *ToScan, selector P);
 
 
 
--- clang/lib/3C/Duplicate.cpp
+++ clang/lib/3C/Duplicate.cpp
@@ -2,30 +2,51 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
 #include "clang/3C/ProgramInfo.h"
-#include "clang/AST/Stmt.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 
 using namespace clang;
 using namespace llvm;
+using namespace std;
 
 
+// Recursive Visitor
 class FindVarDecl : public RecursiveASTVisitor<FindVarDecl> {
 public:
   explicit FindVarDecl(ASTContext &Context, Rewriter &R,
                        ProgramInfo &I, selector P)
       : Context(Context),  R(R), Info(I), P(P) {}
 
+
   bool VisitVarDecl(VarDecl *VD) {
+    // If the selctor matches execute the duplicate function
+    // and store the new name in the optional
     if(P(VD)){
-      addNewVD(VD);
+      auto NewName = addDuplicate(VD);
+      Replacement = { NewName };
       return false;
     } else {
       return true;
     }
   }
 
-  //TODO handle mixed type multi-decl
-  std::string createDuplicateString(VarDecl *VD) {
+  Option<string> didReplace(void) {
+    return Replacement;
+  }
+
+private:
+  // Insert the duplicate into the rewriter
+  // Returns the new var's name
+  string addDuplicate(VarDecl *VD) {
+    SourceLocation End = VD->getEndLoc();
+    auto Dup = createDuplicateString(VD);
+    R.InsertTextAfterToken(End, Dup.second);
+    return Dup.first;
+  }
+
+  // Create the text for the new variable
+  // Returns a pair containing the new name
+  // and the new line containing declaration and assignment
+  pair<string,string> createDuplicateString(VarDecl *VD) {
     auto CV = Info.getVariable(VD, &Context);
     auto Type =
         CV.hasValue() ?
@@ -35,26 +56,25 @@ public:
                       : VD->getType().getAsString();
     auto TargetName = VD->getNameAsString();
     auto NewName = "__" + TargetName + "_copy";
-    return ";\n" + Type + " " + NewName + "= " + TargetName + "";
-  }
-
-  void addNewVD(VarDecl *VD) {
-    SourceLocation End = VD->getEndLoc();
-    R.InsertTextAfterToken(End, createDuplicateString(VD));
+    return make_pair(NewName, ";\n" + Type + " " + NewName + "= " + TargetName + "");
   }
 
 
-private:
+
   ASTContext &Context;
   Rewriter &R;
   ProgramInfo &Info;
+  // Predicate for finding the variable to duplicate
   selector P;
-
+  // Duplicate's name or none if var not found
+  Option<string> Replacement = {};
 };
 
-void create_duplicate(ASTContext &Context, Rewriter &R,
+// Main entrypoint
+Option<string> createDuplicate(ASTContext &Context, Rewriter &R,
                       ProgramInfo &Info,
                       Decl *FD, selector P) {
   auto V = FindVarDecl(Context, R, Info, P);
   V.TraverseDecl(FD);
+  return V.didReplace();
 }
