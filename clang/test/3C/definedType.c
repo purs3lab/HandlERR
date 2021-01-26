0481898b6c29a2d7c47cf3112ffb57498f43de67
Aaron Eline <aeline@umd.edu>
2021-01-26 17:50:53 -0500

    Moved definedType test to count

--- clang/test/3C/definedType.c
+++ clang/test/3C/definedType.c
@@ -2,7 +2,7 @@
 // RUN: 3c -addcr %s -- | FileCheck -match-full-lines -check-prefixes="CHECK_NOALL","CHECK" %s
 // RUN: 3c -addcr %s -- | %clang -c -fcheckedc-extension -x c -o /dev/null -
 // RUN: 3c -alltypes -output-postfix=checked %s
-// RUN: 3c -alltypes %S/definedType.checked.c | diff %S/definedType.checked.c -
+// RUN: 3c -alltypes %S/definedType.checked.c | count 0
 // RUN: rm %S/definedType.checked.c
 
 #include <stddef.h>
