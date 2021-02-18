edacff6e26328d2c5b29446373aea43031dfb39c
Kyle Headley <kyle@correctcomputation.com>
2021-02-18 15:35:29 -0500

    remove xfail from tests, add grep

--- clang/test/3C/difftypes2.c
+++ clang/test/3C/difftypes2.c
@@ -1,7 +1,6 @@
 // RUN: rm -rf %t*
-// RUN: 3c -base-dir=%S -output-dir=%t.checked %s --
-
-// XFAIL: *
+// RUN: not 3c -base-dir=%S -output-dir=%t.checked %s -- 2>%t.stderr
+// RUN: grep -q "merging failed" %t.stderr
 
 // The desired behavior in this case is to fail, so other checks are omitted
 
