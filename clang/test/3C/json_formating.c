4beb96b4ffd6055f72ee438d7a831aebb05d20bf
John Kastner <john@correctcomputation.com>
2021-06-09 13:24:40 -0400

    Fix json syntax in TotalConstraintStats.json
    An extra comma was added in this file when running 3c without -alltypes.
    Also adds a test case that checks the syntax in all generated json files.

---
+++ clang/test/3C/json_formating.c
@@ -0,0 +1,8 @@
+// RUN: 3c -base-dir=%S -alltypes -dump-stats -dump-intermediate -debug-solver %s
+// RUN: python -c "import json, glob; [json.load(open(f)) for f in glob.glob('*.json')]"
+// RUN: 3c -base-dir=%S -dump-stats -dump-intermediate -debug-solver %s
+// RUN: python -c "import json, glob; [json.load(open(f)) for f in glob.glob('*.json')]"
+
+// Testing that json files output for statistics logging are well formed
+
+int *a;
