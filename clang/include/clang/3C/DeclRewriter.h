cbc36a931e073fb413bbc839be512b7aac2bfb6d
John Kastner <john@correctcomputation.com>
2021-06-18 17:37:19 -0400

    Code cleanup

--- clang/include/clang/3C/DeclRewriter.h
+++ clang/include/clang/3C/DeclRewriter.h
@@ -35,6 +35,11 @@ public:
   // Info parameter are rewritten.
   static void rewriteDecls(ASTContext &Context, ProgramInfo &Info, Rewriter &R);
 
+  static void
+  buildItypeDecl(PVConstraint *Defn, DeclaratorDecl *Decl, std::string &Type,
+                 std::string &IType, ProgramInfo &Info,
+                 ArrayBoundsRewriter &ABR);
+
 private:
   static RecordDecl *LastRecordDecl;
   static std::map<Decl *, Decl *> VDToRDMap;
