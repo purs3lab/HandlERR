800e265e005672386556000ae4c760b3edf99952
John Kastner <john@correctcomputation.com>
2021-07-14 12:57:37 -0400

    Tweaks to StructAccessVisitor
    * Make two fields private. One of these was never used publicly, the
      other is exposed through a getter.
    * Slightly simpler logic for handling variable declarations.
      `isLocalVarDecl` returns false for ParmVarDecls, so it's OK to update
      IsGlobal with the result of this function for ParmVarDecl and
      VarDecl.
    * Added note about a bug on structure access such as
      `( 0 ? global_struct_var : local_struct_var).array`.

--- clang/lib/3C/CtxSensAVarBounds.cpp
+++ clang/lib/3C/CtxSensAVarBounds.cpp
@@ -10,7 +10,6 @@
 // CtxSensAVarBounds.h
 //
 //===----------------------------------------------------------------------===//
-
 #include "clang/3C/CtxSensAVarBounds.h"
 #include "clang/3C/AVarBoundsInfo.h"
 #include "clang/3C/ConstraintResolver.h"
@@ -23,31 +22,30 @@
 // We will ignore array indexing.
 class StructAccessVisitor : public RecursiveASTVisitor<StructAccessVisitor> {
 public:
-  std::vector<std::string> StructAccessStr;
-  bool IsGlobal = false;
-
-  explicit StructAccessVisitor(ASTContext *Ctx) : C(Ctx) {
-    StructAccessStr.clear();
-  }
+  explicit StructAccessVisitor(ASTContext *Ctx) : C(Ctx), StructAccessStr() {}
   virtual ~StructAccessVisitor() { StructAccessStr.clear(); }
 
+  bool isGlobal() const { return IsGlobal; }
+
   void processVarDecl(VarDecl *VD) {
-    if (VD != nullptr &&
-        (VD->getType()->isPointerType() || VD->getType()->isStructureType())) {
+    assert("StructAccessVisitor visiting null VarDecl" && VD != nullptr);
+    if (VD->getType()->isPointerType() || VD->getType()->isStructureType()) {
+      // If VD is a ParmVarDecl isLocalVarDecl will return false
+      // FIXME: This method can be called multiple times, and each time it will
+      //        set the value of IsGlobal. This could be a problem if it is
+      //        called on a global and then a local variable. e.g.:
+      //        ( 0 ? global_struct_var : local_struct_var).array
+      //        IsGlobal might be true or false depending on the order it
+      //        encounters the variables. This would also change the order of
+      //        entries in StructAccessStr.
       IsGlobal = !VD->isLocalVarDecl();
       StructAccessStr.insert(StructAccessStr.begin(), VD->getNameAsString());
     }
   }
 
   bool VisitDeclRefExpr(DeclRefExpr *DRE) {
-    ParmVarDecl *PD = dyn_cast_or_null<ParmVarDecl>(DRE->getDecl());
-    if (PD != nullptr &&
-        (PD->getType()->isPointerType() || PD->getType()->isStructureType())) {
-      StructAccessStr.insert(StructAccessStr.begin(), PD->getNameAsString());
-    } else {
-      VarDecl *VD = dyn_cast_or_null<VarDecl>(DRE->getDecl());
+    if (auto *VD = dyn_cast_or_null<VarDecl>(DRE->getDecl()))
       processVarDecl(VD);
-    }
     return true;
   }
 
@@ -59,7 +57,7 @@ public:
   }
 
   // This gives us a string serves as a key for a struct member access.
-  std::string getStructAccessKey() {
+  std::string getStructAccessKey() const {
     std::string Ret = "";
     for (auto CurrStr : StructAccessStr) {
       Ret = CurrStr + ":" + Ret;
@@ -69,6 +67,8 @@ public:
 
 private:
   ASTContext *C;
+  bool IsGlobal = false;
+  std::vector<std::string> StructAccessStr;
 };
 
 void CtxSensitiveBoundsKeyHandler::insertCtxSensBoundsKey(
@@ -146,7 +146,7 @@ CtxStKeyMap *CtxSensitiveBoundsKeyHandler::getCtxStKeyMap(MemberExpr *ME,
                                                           ASTContext *C) {
   StructAccessVisitor SKV(C);
   SKV.TraverseStmt(ME->getBase()->getExprStmt());
-  CtxStKeyMap *MECSMap = getCtxStKeyMap(SKV.IsGlobal);
+  CtxStKeyMap *MECSMap = getCtxStKeyMap(SKV.isGlobal());
   return MECSMap;
 }
 
@@ -238,9 +238,9 @@ void CtxSensitiveBoundsKeyHandler::contextualizeCVar(MemberExpr *ME,
       SKV.TraverseStmt(ME->getBase()->getExprStmt());
       std::string AK = SKV.getStructAccessKey();
       // Context sensitive struct key map.
-      CtxStKeyMap *MECSMap = getCtxStKeyMap(SKV.IsGlobal);
+      CtxStKeyMap *MECSMap = getCtxStKeyMap(SKV.isGlobal());
       auto &BKeyMap = (*MECSMap)[AK];
-      contextualizeStructRecord(I, C, RD, AK, BKeyMap, SKV.IsGlobal);
+      contextualizeStructRecord(I, C, RD, AK, BKeyMap, SKV.isGlobal());
     }
   }
 }
@@ -259,10 +259,10 @@ void CtxSensitiveBoundsKeyHandler::contextualizeCVar(VarDecl *VD, ASTContext *C,
     StructAccessVisitor SKV(C);
     SKV.processVarDecl(VD);
     // Context sensitive struct key map.
-    CtxStKeyMap *MECSMap = getCtxStKeyMap(SKV.IsGlobal);
+    CtxStKeyMap *MECSMap = getCtxStKeyMap(SKV.isGlobal());
     std::string AK = SKV.getStructAccessKey();
     auto &BKeyMap = (*MECSMap)[AK];
-    contextualizeStructRecord(I, C, RD, AK, BKeyMap, SKV.IsGlobal);
+    contextualizeStructRecord(I, C, RD, AK, BKeyMap, SKV.isGlobal());
   }
 }
 
@@ -366,7 +366,7 @@ bool ContextSensitiveBoundsKeyVisitor::VisitDeclStmt(DeclStmt *DS) {
           // to various fields of the structure variable.
           const RecordDecl *Definition =
               ILE->getType()->getAsStructureType()->getDecl()->getDefinition();
-          auto *CSKeyMap = CSBHandler.getCtxStKeyMap(SAV.IsGlobal);
+          auto *CSKeyMap = CSBHandler.getCtxStKeyMap(SAV.isGlobal());
           unsigned int InitIdx = 0;
           const auto Fields = Definition->fields();
           for (auto It = Fields.begin();
