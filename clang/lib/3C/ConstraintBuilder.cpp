9af4802dd9c5b9c8bf4e465f78ce152f81cbd435
Kyle Headley <kyle@correctcomputation.com>
2021-08-12 10:30:56 -0400

    Revert "avoid adding extra constraint when casting arguments to void"
    This reverts commit f5cabd067072ce249f5439beab5ad905e3db45c7.
    
    It was prematurly added from another branch that may not be used.

--- clang/lib/3C/ConstraintBuilder.cpp
+++ clang/lib/3C/ConstraintBuilder.cpp
@@ -244,11 +244,12 @@ public:
           for (const auto &A : E->arguments()) {
             CSetBkeyPair ArgumentConstraints;
             if (I < TargetFV->numParams()) {
-              // When the function has a void* parameter, Clang will
-              // add an implicit cast to void* here. Generating constraints
-              // will add an extraneous wild constraint to void*. This
-              // unnecessarily complicates results and root causes.
-              if (TargetFV->getExternalParam(I)->isVoidPtr())
+              // Remove casts to void* on polymorphic types that are used
+              // consistently.
+              const int TyIdx =
+                  TargetFV->getExternalParam(I)->getGenericIndex();
+              if (ConsistentTypeParams.find(TyIdx) !=
+                  ConsistentTypeParams.end())
                 ArgumentConstraints =
                     CB.getExprConstraintVars(A->IgnoreImpCasts());
               else
