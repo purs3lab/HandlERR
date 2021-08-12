d967becd824f9c482895a7cf40fa6ca5e1c3e7ef
Aaron Eline <aaron@correctcomputation.com>
2021-08-12 10:46:31 -0400

    Removing unnecessary function from test

--- clang/test/3C/base_subdir/unwritable_rootcauses.c
+++ clang/test/3C/base_subdir/unwritable_rootcauses.c
@@ -2,6 +2,3 @@
 // RUN: 3c -use-malloc=my_malloc -alltypes -addcr -output-dir=%t.checked/base_subdir -warn-all-root-cause %s -- -Xclang -verify=unwritable-expected -Wno-everything
 
 #include "../root_cause.c"
-
-void foo(void) { 
-}
