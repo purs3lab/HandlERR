72be995e51a11b94c40f4c3ef83ff2e761b7175e
Shank <shashanksharma21@gmail.com>
2022-03-28 11:55:52 -0400

    Move visitorutil to header itself

--- clang/include/clang/DetectERR/VisitorUtils.h
+++ clang/include/clang/DetectERR/VisitorUtils.h
@@ -7,6 +7,32 @@
 /// Visitor
 template <typename Visitor>
 void addErrorGuards(std::vector<std::pair<Stmt *, CFGBlock *>> &Checks,
-                    Stmt *ReturnST, Visitor &T);
+                    Stmt *ReturnST, Visitor &T) {
+  for (unsigned long I = 0; I < Checks.size(); I++) {
+    if (I == 0) {
+      SourceRange CurrSR = Checks[I].first->getSourceRange();
+      SourceRange ReturnSTSR = ReturnST->getSourceRange();
+      GuardLevel Lvl = GuardLevel::Default;
+      if (CurrSR.fullyContains(ReturnSTSR)) {
+        Lvl = GuardLevel::Inner;
+      }
+      T.Info.addErrorGuardingStmt(T.FID, Checks[I].first, ReturnST, T.Context,
+                                  T.Heuristic, Lvl);
+    } else {
+      // conditions for a guard to be "Outer"
+      // - return statement is within it
+      // - inner guard is within it
+      SourceRange CurrSR = Checks[I].first->getSourceRange();
+      SourceRange InnerSR = Checks[0].first->getSourceRange();
+      SourceRange ReturnSTSR = ReturnST->getSourceRange();
+      GuardLevel Lvl = GuardLevel::Default;
+      if (CurrSR.fullyContains(InnerSR) && CurrSR.fullyContains(ReturnSTSR)) {
+        Lvl = GuardLevel::Outer;
+      }
+      T.Info.addErrorGuardingStmt(T.FID, Checks[I].first, ReturnST, T.Context,
+                                  T.Heuristic, Lvl);
+    }
+  }
+}
 
 #endif //LLVM_CLANG_DETECTERR_VISITOR_UTILS_H
